0d884265542786edd9afbc6b227bfff4cec49b23
Mierukochannn <anjara0003@gmail.com>
2025-05-30 22:34:51 +0300

    Update uptime.js

--- scripts/cmds/uptime.js
+++ scripts/cmds/uptime.js
@@ -1,28 +1,21 @@
 module.exports = {
-  config: {
-    name: "up",
-    aliases: ["up", "upt"],
-    version: "1.0",
-    author: "NiSaN",
-    role: 0,
-    shortDescription: {
-      en: "Displays the uptime of the bot."
-    },
-    longDescription: {
-      en: "Displays the amount of time that the bot has been running for."
-    },
-    category: "System",
-    guide: {
-      en: "Use {p}uptime to display the uptime of the bot."
-    }
-  },
-  onStart: async function ({ api, event, args }) {
-    const uptime = process.uptime();
-    const seconds = Math.floor(uptime % 60);
-    const minutes = Math.floor((uptime / 60) % 60);
-    const hours = Math.floor((uptime / (60 * 60)) % 24);
-    const days = Math.floor(uptime / (60 * 60 * 24));
-    const uptimeString = `╭──⩸__ 𝙽𝙸 𝚂 𝙰𝙽 𝙱𝚘𝚃__⩸ ☔︎𝚄𝙿𝚃𝚒𝚖𝚎⏳👈\n├⏳ 𝙷𝙾𝚄𝚁𝚂 ${hours}\n├⏰ 𝙼𝙸𝙽𝚄𝚃𝙴𝚂 ${minutes}\n├⏲️ 𝚂𝙴𝙲𝙾𝙽𝙳𝚂 ${seconds}\n╰───────────✰`;
-    api.sendMessage(`${uptimeString}`, event.threadID);
-  }
+ config: {
+ name: "☕",
+ version: "1",
+ author: "thea",
+ countDown: 5,
+ role: 0,
+ shortDescription: "aesther coffe pic💬",
+ longDescription: "no prefix",
+ category: "no prefix",
+ }, 
+ onStart: async function(){}, 
+ onChat: async function({ event, message, getLang }) {
+ if (event.body && event.body.toLowerCase() === "☕") {
+ return message.reply({
+ body: "「☕」- 😌",
+ attachment: await global.utils.getStreamFromURL("https://i.postimg.cc/fyLQW03x/e6f8ef910d59ff3001b5b5151f59c8aa.jpg")
+ });
+ }
+ }
 }
